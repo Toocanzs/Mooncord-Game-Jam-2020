d03bc09108e5f313198c356c2b86079058be44dd
sacha roberts <32625963+sacharobarts@users.noreply.github.com>
2020-05-29 13:06:08 -0700

    first-pass character movement w/dash and horizontal sprite flipping

---
+++ Assets/Scripts/Entities/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float move_speed;
+    public float dash_velocity;
+    public float dash_duration;
+    public float dash_cooldown;
+
+    private SpriteRenderer character_sprite;
+    private Rigidbody2D rigid_body;
+    private float dash_cooldown_remaining;
+    private float dash_active_time_remaining;
+    private Vector2 dash_direction;
+
+    void Start()
+    {
+        rigid_body = GetComponent<Rigidbody2D>();
+        character_sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateMovement();
+        SetSpriteFacing();
+
+    }
+
+    private void UpdateMovement() {
+        var axis_horizontal = Input.GetAxis("Horizontal");
+        var axis_vertical = Input.GetAxis("Vertical");
+        dash_cooldown_remaining = Mathf.Max(dash_cooldown_remaining - Time.deltaTime, 0f);
+        dash_active_time_remaining = Mathf.Max(dash_active_time_remaining - Time.deltaTime, 0f);
+        if(dash_active_time_remaining > 0f) {
+            rigid_body.velocity = dash_direction * dash_velocity;
+        } else if (Input.GetKey("space") && dash_cooldown_remaining == 0f) {
+            var direction = new Vector2(axis_horizontal, axis_vertical);
+            dash_direction = direction.normalized;
+            rigid_body.velocity = dash_direction * dash_velocity;
+            dash_cooldown_remaining = dash_cooldown;
+            dash_active_time_remaining = dash_duration;
+        } else {
+            rigid_body.velocity = new Vector2(axis_horizontal * move_speed, axis_vertical * move_speed);
+        }
+
+    }
+
+    private void SetSpriteFacing() {
+        var axis_horizontal = Input.GetAxis("Horizontal");
+        var axis_vertical = Input.GetAxis("Vertical");
+        if(axis_horizontal > 0f) {
+            character_sprite.flipX = false;
+        } else if(axis_horizontal < 0f) {
+            character_sprite.flipX = true;
+        }
+        if(axis_vertical > 0f) {
+
+        }
+    }
+}
