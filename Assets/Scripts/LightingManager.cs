98a0ae7ea6de4f3d6983cf97434d5d0324a63336
Toocanzs <dupanzszs@gmail.com>
2020-05-29 15:58:28 -0400

    add some basic placeholder sprites to test with

---
+++ Assets/Scripts/LightingManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class LightingManager : Singleton<LightingManager>
+{
+    public Camera lightingCamera;
+
+    public int PixelsPerUnit = 32;
+    public int2 ProbeCounts = new int2(50, 30);
+    void Start()
+    {
+        lightingCamera = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        
+    }
+}
