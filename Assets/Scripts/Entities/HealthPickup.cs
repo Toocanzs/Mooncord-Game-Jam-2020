f1221dee815d1d4dfbede2bc748bb29e09d54906
sacha roberts <32625963+sacharobarts@users.noreply.github.com>
2020-05-29 14:10:08 -0700

    simple pickup system + health pickup prefab

---
+++ Assets/Scripts/Entities/HealthPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour, IPikcupable
+{
+    public float health_amount;
+
+    public void OnCharacter(GameObject character_gameobject) {
+        var player_health_component = character_gameobject.GetComponent<HealthComponent>();
+        player_health_component.ChangeHealth(health_amount);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Entities/Pickupable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    public bool destroy_owner_on_pickup = true;
+    private Collider2D trigger;
+
+    private void Start() {
+        trigger = GetComponent<Collider2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.GetComponent<PlayerCharacter>()) {
+            var pickup_interface = GetComponent<IPikcupable>();
+            if(pickup_interface != null) {
+                pickup_interface.OnCharacter(other.gameObject);
+                if (destroy_owner_on_pickup) {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+
+
+}
+
+public interface IPikcupable {
+    void OnCharacter(GameObject character_gameobject);
+}
