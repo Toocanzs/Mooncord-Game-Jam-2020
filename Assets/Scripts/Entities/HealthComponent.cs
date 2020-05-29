ff0560de6060f5c7fb1409ac8f535dac769e3ca2
sacha roberts <32625963+sacharobarts@users.noreply.github.com>
2020-05-29 13:42:14 -0700

    simple collision setup. adding simple health component

---
+++ Assets/Scripts/Entities/HealthComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public float max_health;
+    private float current_health;
+
+    public float Health {
+        get { return current_health; }
+    }
+
+    void Start()
+    {
+        current_health = max_health;
+    }
+
+    public void ChangeHealth(float value) {
+        current_health = Mathf.Clamp(current_health + value, 0f, max_health);
+    }
+
+    public bool isDead() {
+        return current_health == 0f;
+    }
+       
+}
