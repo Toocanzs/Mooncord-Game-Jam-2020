b8fb4355831dd5f1c716e734e95af4a1aea35eb7
Toocanzs <dupanzszs@gmail.com>
2020-05-29 20:35:43 -0400

    Add pathfinding class

--- Assets/Scripts/PathfindingManager.cs
+++ Assets/Scripts/PathfindingManager.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using Unity.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -8,6 +12,18 @@ public class PathfindingManager : MonoBehaviour
 
     public Tilemap OccluderTilemap;
     public static PathfindingManager Instance;
+
+    private float timeSinceLastGrabbedTiles = 0;
+    public float tileUpdateInterval = 0.25f;
+    private TileBase[] tiles;
+
+    struct TilePathfindingData
+    {
+        public float2 closestSeen;
+        public int distance;
+    }
+
+    private NativeArray<TilePathfindingData> tileData;
     void Start()
     {
         if (Instance != null)
@@ -19,13 +35,31 @@ public class PathfindingManager : MonoBehaviour
 
         Instance = this;
     }
+
+    private void OnDestroy()
+    {
+        if (tileData.IsCreated)
+        {
+            tileData.Dispose();
+        }
+    }
+
     void Update()
     {
-        OccluderTilemap.GetTilesBlock(OccluderTilemap.cellBounds);
+        if (tiles == null || timeSinceLastGrabbedTiles > tileUpdateInterval)
+        {
+            tiles = OccluderTilemap.GetTilesBlock(OccluderTilemap.cellBounds);
+            if (tiles.Length != tileData.Length)
+            {
+                if (tileData.IsCreated)
+                {
+                    tileData.Dispose();
+                }
+                tileData = new NativeArray<TilePathfindingData>(tiles.Length, Allocator.Persistent);
+            }
+            timeSinceLastGrabbedTiles = 0;
+        }
+       
+        timeSinceLastGrabbedTiles += Time.deltaTime;
     }
-}
-
-public static class TilemapExtensions
-{
-    //this.GetTileAssetsBlock(bounds.min, bounds.size)
 }
