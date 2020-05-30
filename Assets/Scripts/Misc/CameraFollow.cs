cee6e24af9c0ef1a4643af77e31db5fd35d2bd0b
sacha roberts <32625963+sacharobarts@users.noreply.github.com>
2020-05-29 17:42:38 -0700

    adding camera follow w/delay

---
+++ Assets/Scripts/Misc/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target_transform;
+    public float FollowDelay;
+
+    private Vector3 followVelocity;
+    //private Vector3 finalTarget;
+
+    //public Vector3 FollowPosition { get { return finalTarget; } }
+
+    public void SetTarget(Transform target) {
+        target_transform = target;
+    } 
+
+    private void FixedUpdate()
+    {
+        TrackTarget();
+    }
+
+    private void TrackTarget()
+    {
+        Vector2 targetPos = new Vector2(target_transform.position.x, target_transform.position.y);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 offset = mousePos - targetPos;
+        Vector2 direction = offset.normalized;
+
+        Vector2 target = (Vector2)target_transform.position + (offset / 2.5f);
+
+        var final_target = new Vector3(target.x, target.y, -10f);
+        transform.position = Vector3.SmoothDamp(transform.position, final_target, ref followVelocity, FollowDelay);
+    }
+
+}
