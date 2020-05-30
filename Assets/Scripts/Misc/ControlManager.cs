3ddddab3fd22afc169ff9dafc8b2339539210ea7
Toocanzs <dupanzszs@gmail.com>
2020-05-30 18:12:05 -0400

    add a ton of sounds

---
+++ Assets/Scripts/Misc/ControlManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlManager : MonoBehaviour
+{
+    private bool player_input_enabled;
+    private static ControlManager instance;
+
+    private void Awake() {
+        if (!instance) {
+            instance = this;
+        }
+        // @TEMP;
+        player_input_enabled = true;
+    }
+
+    public static bool IsInputEnabled() {
+        return instance.player_input_enabled;
+    }
+
+    public static void SetInputEnabled(bool value) {
+        instance.player_input_enabled = value;
+    }
+}
---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpawnDirection { NORTH, EAST, SOUTH, WEST };
+
+public class PlayerSpawn : MonoBehaviour
+{
+    public SpawnDirection spawn_direction;
+}
