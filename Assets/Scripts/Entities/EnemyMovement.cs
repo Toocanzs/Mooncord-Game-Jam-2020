65808e364c0b9dcd91ae7dc27b6b084d61e33e15
sacha roberts <32625963+sacharobarts@users.noreply.github.com>
2020-05-31 00:26:32 -0700

    nanite behavior and animation stuffs....

--- Assets/Scripts/Entities/EnemyMovement.cs
+++ Assets/Scripts/Entities/EnemyMovement.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyMovement : CharacterMovement
 {
-    public float speed;
 
     private Rigidbody2D rigid_body;
 
-    private void Awake() {
+    protected override void Awake() {
+        base.Awake();
         rigid_body = GetComponent<Rigidbody2D>();
     }
 
     public void MoveDirection(Unity.Mathematics.float2 direction) {
-        rigid_body.velocity = direction * speed;
+        rigid_body.velocity = direction * move_speed;
     }
 
 }
