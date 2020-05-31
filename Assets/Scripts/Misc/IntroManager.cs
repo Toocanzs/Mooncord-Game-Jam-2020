ae148eb64fa84d9f0a1516ace23746daf2eec974
sacha roberts <32625963+sacharobarts@users.noreply.github.com>
2020-05-30 17:41:35 -0700

    menu -> intro  -> game sequence pretty set (need content)

--- Assets/Scripts/Misc/IntroManager.cs
+++ Assets/Scripts/Misc/IntroManager.cs
@@ -2,14 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System;
 
 public class IntroManager : MonoBehaviour
 {
+    public List<GameObject> intro_text = new List<GameObject>();
+    private int intro_index;
     private float skip_held_time;
     private SkipIndicator skip_indicator_component;
+    private GameObject current_intro_text_intance;
 
     private void Start() {
-        skip_indicator_component = Object.FindObjectOfType<SkipIndicator>();
+        skip_indicator_component = FindObjectOfType<SkipIndicator>();
+        StartIntro();
+    }
+
+    private void StartIntro() {
+        SpawnNextIntroElement();
+    }
+
+    private void SpawnNextIntroElement() {
+        if (intro_index < intro_text.Count) {
+            var canvas_component = FindObjectOfType<Canvas>();
+            if (!canvas_component) {
+                Debug.LogError("Missing canvas component in scene!!");
+                return;
+            }
+            if (current_intro_text_intance) {
+                Destroy(current_intro_text_intance);
+            }
+            current_intro_text_intance = Instantiate(intro_text[intro_index], canvas_component.gameObject.transform);
+            var text_element_component = current_intro_text_intance.GetComponent<TextElement>();
+            if (!text_element_component) {
+                Debug.LogWarning("Unable to find TextElement component on intro text instance");
+                return;
+            }
+            text_element_component.OnComplete += (caller, args) => { SpawnNextIntroElement(); };
+            text_element_component.StartHidden(true);
+            var text_length = text_element_component.GetTextLength();
+            text_element_component.RevealText(text_length / 15);
+            intro_index++;
+        } else {
+            Destroy(current_intro_text_intance);
+            EndIntro();
+        }
     }
 
     private void Update() {
---
+++ Assets/Scripts/UI/TextElement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using System;
+
+public class TextElement : MonoBehaviour
+{
+
+    public bool start_hidden;
+    private TextMeshProUGUI text_component;
+    private bool next_pressed;
+    private bool is_revealing;
+    private DG.Tweening.Core.TweenerCore<int, int, DG.Tweening.Plugins.Options.NoOptions> reveal_tween;
+    public event EventHandler OnComplete;
+
+    void Awake()
+    {
+        text_component = GetComponentInChildren<TextMeshProUGUI>();
+        if (start_hidden) {
+            text_component.maxVisibleCharacters = 0;
+            is_revealing = true;
+        }
+    }
+
+    public void StartHidden(bool value) {
+        text_component.maxVisibleCharacters = value ? 0 : text_component.text.Length;
+        is_revealing = value;
+    }
+
+    public int GetTextLength() {
+        return text_component.text.Length;
+    }
+
+    public void RevealText(float time, System.Action callback = null) {
+        var char_count = text_component.text.Length;
+        if(time == 0f) {
+            text_component.maxVisibleCharacters = char_count;
+            if (callback != null) {
+                callback.Invoke();
+            }
+            return;
+        }
+        reveal_tween = DOTween.To(() => text_component.maxVisibleCharacters, x => text_component.maxVisibleCharacters = x, char_count, time).OnComplete(() => {
+            is_revealing = false;
+            if (callback != null) {
+                callback.Invoke();
+            }
+            reveal_tween = null;
+        });
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            next_pressed = true;
+        }
+        if(!Input.GetKey(KeyCode.Space) && next_pressed) {
+            next_pressed = false;
+            if (is_revealing && reveal_tween != null) {
+                reveal_tween.Complete();
+            } else {
+                OnComplete?.Invoke(this,null);
+            }
+        }
+    }
+}
