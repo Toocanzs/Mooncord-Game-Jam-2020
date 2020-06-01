2edd0921b1dc6358015d74f365a46954263a4733
Toocanzs <dupanzszs@gmail.com>
2020-06-01 09:38:09 -0400

    add ending scene

---
+++ Assets/EndingScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class EndingScript : MonoBehaviour
+{
+    private float time = 0;
+    public List<Image> images;
+    [FormerlySerializedAs("curve")]
+    public AnimationCurve reunitedCurve;
+    public Image fade;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float v = reunitedCurve.Evaluate(time);
+        foreach (var image in images)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, v);
+        }
+        fade.color = new Color(0,0,0, Mathf.Clamp01(2-time));
+
+        time += Time.deltaTime;
+    }
+}
