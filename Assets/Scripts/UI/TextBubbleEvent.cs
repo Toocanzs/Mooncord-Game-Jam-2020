85f80458d744a8c163355d9948c1b35f84c1e8da
sacha roberts <32625963+sacharobarts@users.noreply.github.com>
2020-06-01 09:05:30 -0700

    adding space hint to progress game intro

--- Assets/Scripts/UI/TextBubbleEvent.cs
+++ Assets/Scripts/UI/TextBubbleEvent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class TextBubbleEvent : MonoBehaviour, ISequenceEvent
     public bool attach_to_character;
     public bool attach_to_location;
     public Vector3 attach_location;
+    public SpriteRenderer hint_sprite_renderer;
 
     public void StartEvent(Action complete_callback) {
         var bubble_component = GetComponentInChildren<TextBubble>();
@@ -23,5 +25,16 @@ public class TextBubbleEvent : MonoBehaviour, ISequenceEvent
         var text_length = text_element.GetTextLength();
         text_element.RevealText(text_length / 15);
         text_element.OnComplete += (inst, args) => { complete_callback?.Invoke(); };
+
+        if(hint_sprite_renderer != null) {
+            var seq = DOTween.Sequence();
+            seq.SetDelay(2.0f);
+            seq.Append(hint_sprite_renderer.DOColor(new Color(1f, 1f, 1f, 1f), 0.4f));
+            seq.OnComplete(() => seq = null);
+        }
+    }
+
+    private void OnDestroy() {
+        DOTween.Kill(this);
     }
 }
