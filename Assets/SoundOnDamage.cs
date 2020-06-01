1d2645c3f8808207e94183263ca8a4453dec00e7
Toocanzs <dupanzszs@gmail.com>
2020-06-01 12:02:48 -0400

    add hit sounds

---
+++ Assets/SoundOnDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource), typeof(HealthComponent))]
+public class SoundOnDamage : MonoBehaviour
+{
+    public AudioClip sound;
+    private AudioSource audioSource;
+    private HealthComponent healthComponent;
+
+    private bool died = false;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<HealthComponent>();
+        healthComponent.on_health_change += HandleHealthChange;
+    }
+
+    private void HandleHealthChange(int difference)
+    {
+        audioSource.PlayOneShot(sound);
+        if (healthComponent.Health <= 0)
+        {
+            healthComponent.on_health_change -= HandleHealthChange;
+            return;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
